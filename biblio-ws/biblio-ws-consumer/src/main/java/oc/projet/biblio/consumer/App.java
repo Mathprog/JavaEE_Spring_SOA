c035621a64674678f345b54bd838bede13bdd5ed
Mathieu Martinez <mathieu-martinez@live.com>
2018-11-09 15:03:37 +0100

    "Création de l'architeture de base du Web Service de la bibliothèque"

---
+++ biblio-ws/biblio-ws-consumer/src/main/java/oc/projet/biblio/consumer/App.java
@@ -0,0 +1,13 @@
+package oc.projet.biblio.consumer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ biblio-ws/biblio-ws-model/src/main/java/oc/projet/biblio/model/App.java
@@ -0,0 +1,13 @@
+package oc.projet.biblio.model;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
