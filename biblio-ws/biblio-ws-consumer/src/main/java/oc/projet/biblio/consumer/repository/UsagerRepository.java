2876311d65e3521b87e458e77c933d403f05ae9f
Mathieu Martinez <mathieu-martinez@live.com>
2018-11-11 14:49:52 +0100

    modifications de JUnits

--- biblio-ws/biblio-ws-consumer/src/main/java/oc/projet/biblio/consumer/repository/UsagerRepository.java
+++ biblio-ws/biblio-ws-consumer/src/main/java/oc/projet/biblio/consumer/repository/UsagerRepository.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UsagerRepository extends CrudRepository<Usager, Integer> {
 
+    Usager findUsagerByEmail(String email);
 }
