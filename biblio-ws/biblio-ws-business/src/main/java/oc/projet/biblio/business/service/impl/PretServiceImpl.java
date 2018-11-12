3bac9d47846401f464e5a26e3327e3ff698d07de
Mathieu Martinez <mathieu-martinez@live.com>
2018-11-12 19:53:21 +0100

    "Ajout des services liés au Pret."

---
+++ biblio-ws/biblio-ws-business/src/main/java/oc/projet/biblio/business/service/impl/PretServiceImpl.java
@@ -0,0 +1,7 @@
+package oc.projet.biblio.business.service.impl;
+
+import oc.projet.biblio.business.service.PretService;
+
+public class PretServiceImpl implements PretService {
+
+}
---
+++ biblio-ws/biblio-ws-consumer/src/main/java/oc/projet/biblio/consumer/repository/PretRepository.java
@@ -0,0 +1,8 @@
+package oc.projet.biblio.consumer.repository;
+
+import oc.projet.biblio.model.entity.Pret;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PretRepository extends CrudRepository<Pret, Integer> {
+
+}
